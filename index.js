60fc26e1396724c4498036aa13ad15edd7997b7b
Lazar Andric <Lazarandric97@gmail.com>
2022-02-25 22:05:06 +0100

    Same as last one commit

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-import swaggerUi from 'swagger-ui-express'
-import swaggerDocs from 'swagger-jsdoc'
+// import swaggerUi from 'swagger-ui-express'
+// import swaggerJSDoc from 'swagger-jsdoc'
 
+import authRoute from './routes/authorization.js'
 import usersRoute from './routes/users.js'
-import productsRoute from './routes/users.js'
-import swaggerJSDoc from 'swagger-jsdoc'
+import productsRoute from './routes/products.js'
 
-const options={
-  definition: {
-    openapi: '3.0.0',
-    info:{
-      title: "Bauk Cheescake",
-      version: "0.0.1",
-      description:"Api for sale cheescakes"
-    },
-    servers:[
-      {
-        url: 'https://localhost:4000'
-      }
-    ],
-    apis:['./routes/*.js']
-  }
-}
+// const options={
+//   apis:['./routes/*.js'],
+//   definition: {
+//     openapi: '3.0.0',
+//     info:{
+//       title: "Bauk Cheescake",
+//       version: "0.0.1",
+//       description:"Api for sale cheescakes"
+//     },
+//     servers:[
+//       {
+//         url: 'https://localhost:5000'
+//       }
+//     ],
+//   }
+// }
 
-const specs = swaggerJSDoc(options)
-
+// const specs = swaggerJSDoc(options)
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -35,7 +34,8 @@ app.use(bodyParser.json())
 
 app.use('/products', productsRoute)
 app.use('/users', usersRoute)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+app.use('/authorization', authRoute)
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 
 app.listen(PORT, () => {
