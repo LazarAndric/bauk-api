446416661cd8c95c61e3713a2b9e3b6b67e054ff
landric <lazarandric97@gmail.com>
2022-04-21 13:11:41 +0200

    add log in console

--- routes/places.js
+++ routes/places.js
@@ -23,6 +23,7 @@ router.post('/', userSchema.postPlace, auth.validateInput, async (req,res)=>{
                 AreaCode:req.body.AreaCode,
                 Active: 0
         })
+        console.log(result)
         return errorEventHandler(result).bool ? res.sendStatus(403) : res.sendStatus(201)
 })
 
